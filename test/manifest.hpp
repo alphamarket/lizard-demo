15a6360bd14a287eba710bc20cf21271be6a5ffe
noise2 <b.g.dariush@gmail.com>
2016-03-15 19:13:47 +0330

    + test/

---
+++ test/manifest.hpp
@@ -0,0 +1,19 @@
+#ifndef MANIFEST_HPP
+#define	MANIFEST_HPP
+
+#include "lizard/lizard.hpp"
+
+// include test case files
+//#include "tests/template_tester.hpp"
+
+void lizard_manifest() {
+    /*
+     * use following macros to register test cases
+     *      lizard_register(<test class name>)
+     *      lizard_register_custom(<test label>, <test class name>)
+     * example:
+     *      lizard_register(test::template_tester);
+     */
+}
+
+#endif	/* MANIFEST_HPP */
